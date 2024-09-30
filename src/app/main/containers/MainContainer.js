afee4e8a552e829d8d096e9b2e02cb8b40fac864
beom33 <byby5546@naver.com>
2024-09-30 18:19:35 +0900

    .

---
+++ src/app/main/containers/MainContainer.js
@@ -0,0 +1,28 @@
+'use client'
+import React from "react";
+import { SectionsContainer } from "react-fullpage";
+import TopMenu from "../components/TopMenu";
+import InfoAbout from "../components/InfoAbout";
+import Experience from "../components/Experience";
+import Portfolio from "../components/Portfolio";
+
+
+const options = {
+    anchors: ['sectionOne', 'sectionTwo', 'sectionThree'],
+};
+
+const MainContainer = () => {
+    return (
+    <>
+         <TopMenu/>
+         
+    <SectionsContainer {...options}>
+         <InfoAbout/>
+         <Experience/>
+       
+    </SectionsContainer>
+    </>
+    );
+};
+
+export default React.memo(MainContainer);

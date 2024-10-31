5e1f28ba93fc7141271b103e67f0db85fb7deca3
beom33 <byby5546@naver.com>
2024-11-01 00:06:25 +0900

    Info 정보

--- src/app/main/components/InfoAbout.js
+++ src/app/main/components/InfoAbout.js
@@ -38,8 +38,38 @@ const InfoAbout = () => {
       <Wrapper>
         <ProfileImage />
         <div className="layout-width inner">
-          <Item title="INFO"></Item>
-          <Item title="ABOUT"></Item>
+          <Item title="INFO">
+          <div style={{ display: 'flex', alignItems: 'center', gap: '167px' }}>
+              <p style={{ marginBottom: '55px' }}> YU JUN BEOM</p>
+              <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                <p>MBTI</p>
+                <p style={{ position: 'relative', top: '-15px', opacity: '0.6' }}>ISFP</p>
+              </div>
+            </div>
+            <div style={{ position: 'relative', top: '-57px', opacity: '0.6' }}>
+            <p >1996.10.09</p>
+            </div>
+
+            <div style={{ display: 'flex', alignItems: 'center', gap: '50px' }}>
+              <div>
+              <p>ADRESS</p>
+              <div style={{ position: 'relative', top: '-12px', opacity: '0.6' }}>
+              <p>Gochon-eup, Gimpo-si, Gyeonggi-do</p>
+              </div>
+               </div>
+               
+              <div>
+            <p>PHONE</p>
+           <div style={{ position: 'relative', top: '-12px', opacity: '0.6' }}>
+           <p>010-3738-6542</p>
+         </div>
+           </div>
+              </div>
+
+          </Item>
+          <Item title="ABOUT">
+          <p>ABOUT 섹션에 표시될 내용입니다.</p>
+          </Item>
         </div>
       </Wrapper>
     </Section>
